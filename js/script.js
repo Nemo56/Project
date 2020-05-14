21d004a022f26fcf029f7b0421d711f1f2ee5b5a
Evgeny <nemokom88@mail.ru>
2020-05-14 23:37:21 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+const result = prompt ("Как вас зовут?",'');
+console.log(result);
+
+ 
+alert (`Привет ${result}!`);
+
+//const answer = prompt("Вам есть 18?","18");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt ('Как ваше имя?','');
+//answers[1] = prompt ('Как ваша фамилия?','');
+//answers[2] = prompt ('Сколько вам лет?','');
+
+//console.log(typof(answers));
+//console.log(typeof(null));
